0f0657122a72e9b5db79675b1f44b3697567d954
DruMoDev <dev.morera@gmail.com>
2023-10-23 17:22:25 +0200

    Nuevas claves

--- config/plugins.js
+++ config/plugins.js
@@ -8,9 +8,9 @@ module.exports = ({ env }) => ({
       config: {
         provider: 'cloudinary',
         providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
+          cloud_name: env('CLOUDINARY_NAME', "strapi_1"),
+          api_key: env('CLOUDINARY_KEY', "133642461353478"),
+          api_secret: env('CLOUDINARY_SECRET', "IiR9mc085yhlDEE5EO-jA7jiSjE"),
         },
         actionOptions: {
           upload: {},
