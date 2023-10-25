9bdf41444abce53948d6f0f71b52a86d2badafd0
DruMoDev <dev.morera@gmail.com>
2023-10-25 10:17:55 +0200

    A ver si van las variables

--- config/plugins.js
+++ config/plugins.js
@@ -1,21 +1,22 @@
 module.exports = ({ env }) => ({
-    'users-permissions': {
-      config: {
-      jwtSecret: env('JWT_SECRET'),
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET"),
+    },
+  },
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+        uploadStream: {},
       },
     },
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME', "strapi_1"),
-          api_key: env('CLOUDINARY_KEY', "133642461353478"),
-          api_secret: env('CLOUDINARY_SECRET', "IiR9mc085yhlDEE5EO-jA7jiSjE"),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
-    },
-  });
+  },
+});
